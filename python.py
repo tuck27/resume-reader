b836b3be02dfd375626a00fbf2a36057c58e9f57
Elizabeth <elizabethg2796@tuckahoeschools.org>
2025-03-18 12:29:05 +0000

    init commit

---
+++ python.py
@@ -0,0 +1,39 @@
+'''# importing all the required modules
+import pypdf
+import re
+
+# creating a pdf reader object
+reader = pypdf.PdfReader('Hockey.pdf')
+
+# print the number of pages in pdf file
+print(len(reader.pages))
+
+# print the text of the first page
+print(reader.pages[0].extract_text())
+
+string = "ice"
+
+for page in reader.pages:
+    text = page.extract_text()
+    res_search = re.search(string, text)
+    print(res_search)
+'''
+
+import pypdf
+import re
+
+# open the pdf file
+reader = pypdf.PdfReader("Hockey.pdf")
+
+# get number of pages
+num_pages = len(reader.pages)
+
+# define key terms
+string = "players"
+
+# extract text and do the search
+for page in reader.pages:
+    text = page.extract_text() 
+    # print(text)
+    res_search = re.search(string, text)
+    print(res_search)
