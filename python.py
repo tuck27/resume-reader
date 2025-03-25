19cea5dbefe11b90708f8db415fe8630879ae239
chloe <chloep2838@tuckahoeschools.org>
2025-03-25 12:31:41 +0000

    initial commit

--- python.py
+++ python.py
@@ -1,51 +1,8 @@
-'''# importing all the required modules
-import pypdf
-import re
-
-# creating a pdf reader object
-reader = pypdf.PdfReader('Hockey.pdf')
-
-# print the number of pages in pdf file
-print(len(reader.pages))
-
-# print the text of the first page
-print(reader.pages[0].extract_text())
-
-string = "ice"
-
-for page in reader.pages:
-    text = page.extract_text()
-    res_search = re.search(string, text)
-    print(res_search)
-'''
 
 import pypdf
 import re
 
 # open the pdf file
-reader = pypdf.PdfReader("resume1.pdf")
-
-# get number of pages
-num_pages = len(reader.pages)
-
-# define key terms
-string1 = "Leadership" #college education
-string2 = "university" #skills
-string3 = "Python" #experiences
-string4 = "GPA" #certifications
-
-# extract text and do the search
-for page in reader.pages:
-    text = page.extract_text() 
-    # print(text)
-    res_search = re.findall(string1, text)
-    print(res_search)
-    res_search = re.findall(string2, text)
-    print(res_search)
-    res_search = re.findall(string3, text)
-    print(res_search)
-    res_search = re.findall(string4, text)
-    print(res_search)
 
 
 '''
@@ -76,7 +33,7 @@ pdf = "resume1.pdf"
 
 def get_points_for_pdf(text)-> tuple:
     total_points = 0
-    for word in pdf:
+    for word in text:
         if word == "Leadership":
             total_points += 1
         elif word == "university":
@@ -89,6 +46,6 @@ def get_points_for_pdf(text)-> tuple:
 
 print("Word                 | TOT | AVE ")
 print("---------------------+-----+-----")
-for word in extract_text(reader.pages):
+for word in get_points_for_pdf(text):
     total_points, average_points = get_points_for_pdf(text)
     print(f"{word:20} | {total_points:3} | {average_points:.3}")
