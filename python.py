217213874fa50ea5936d1e6d7cd53432fe704909
Elizabeth <elizabethg2796@tuckahoeschools.org>
2025-03-26 12:46:02 +0000

    finish commit

--- python.py
+++ python.py
@@ -23,23 +23,53 @@ import pypdf
 import re
 
 # open the pdf file
+<<<<<<< HEAD
+=======
+reader = pypdf.PdfReader("resume1.pdf")
 
-#MR COOPER LOOK HERE
-def get_points_for_pdf(text)-> tuple:
-    total_points = 0
-    for word in text:
-        if word == "Leadership":
-            total_points += 1
-        if word == "university":
-            total_points += 1
-        if word == "Python":
-            total_points += 1
-        if word == "GPA":
-            total_points += 1
-    return (total_points, total_points / len(word))
+# get number of pages
+num_pages = len(reader.pages)
 
+# define key terms
+string1 = "leadership" #college education
+string2 = "university" #skills
+string3 = "python" #experiences
+string4 = "GPA" #certifications
+
+# extract text and do the search
+for page in reader.pages:
+    text = page.extract_text() 
+    # print(text)
+    res_search = re.findall(string1, text)
+    print(res_search)
+    res_search = re.findall(string2, text)
+    print(res_search)
+    res_search = re.findall(string3, text)
+    print(res_search)
+    res_search = re.findall(string4, text)
+    print(res_search)
+>>>>>>> 7c49250 (finish commit)
+
+my_dict = {"leadership": 1, "university": 2, "python": 3, "GPA": 4}
+
+<<<<<<< HEAD
 print("Word                 | TOT | AVE ")
 print("---------------------+-----+-----")
 for word in extract_text(reader.pages):
     total_points, average_points = get_points_for_pdf(text)
     print(f"{word:20} | {total_points:3} | {average_points:.3}")
+=======
+def get_points_for_pdf(words):
+    total_points = 0
+    for word in words:
+        if word in my_dict:
+            total_points += my_dict[word]
+    return total_points
+
+# extract text and calculate the score
+for page in reader.pages:
+    text = page.extract_text()
+    words = text.split()  # Split the text into individual words
+    total_points = get_points_for_pdf(words)
+    print(f"Total points for this page: {total_points}")
+>>>>>>> 7c49250 (finish commit)
